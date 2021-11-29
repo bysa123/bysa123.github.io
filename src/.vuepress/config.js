81675f5db1e25457728781ddb4a3e1f321944b6f
heqingqing <heqingqing@tuhu.cn>
2021-11-29 15:45:12 +0800

    change direname

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 module.exports = {
-  title: 'VuePress',
-  dest: path.resolve(_dirname, '../../docs')
+  title: 'vuepress',
+  dest: path.resolve(__dirname, '../../docs'), // 关键是这一行 意思是打包到docs文件夹
 }
+
